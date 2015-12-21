f00162292d7e4cef66c123d06b8b903e309460f8
brother2000years <brthr2000@gmail.com>
2015-12-21 16:44:09 +0300

    Добавлено формирование тестовой задачи

--- FourthLabWork/FourthLabWork/Form1.cs
+++ FourthLabWork/FourthLabWork/Form1.cs
@@ -19,7 +19,32 @@ namespace FourthLabWork
         public Form1()
         {
             InitializeComponent();
+            InitTasks();
         }             
+
+        public double testFunc(Vector2 arg)
+        {
+            return Math.Pow(arg.X, 2) + Math.Pow(arg.Y, 2);
+        }
+
+        public double testLimit(Vector2 arg)
+        {
+            return arg.X + arg.Y - 2;
+        }
+
+        /// <summary>
+        /// Инициализировать задачи 
+        /// </summary>
+        private void InitTasks()
+        {
+            // Тестовая задача 1.1
+            List<Limitation> testTaskLimits = new List<Limitation>();
+            testTaskLimits.Add(new Limitation(testLimit, true, false));            
+            tasks.Add(new MinimizationTask(testTaskLimits, testFunc));
+            // Учебная задача 2.1
+            // Учебная задача 2.2
+            // Учебная задача 2.3
+        }
 
         private void btnCount_Click(object sender, EventArgs e)
         {
