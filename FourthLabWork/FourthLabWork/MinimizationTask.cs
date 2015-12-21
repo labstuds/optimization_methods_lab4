a81ff97976d29696c27993a0181edbab2ed4ecbf
brother2000years <brthr2000@gmail.com>
2015-12-21 16:25:39 +0300

    Добавлены классы ограничений и задачи минимизации

---
+++ FourthLabWork/FourthLabWork/MinimizationTask.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FourthLabWork
+{
+    /// <summary>
+    /// Задача минимизации
+    /// </summary>
+    class MinimizationTask
+    {
+        public Func<Vector2, double> Formula { get; private set; }
+        public bool IsEqual { get; private set; }
+        public bool IsBiggerThanZero { get; private set; }        
+    }
+
+    /// <summary>
+    /// Ограничение для функции
+    /// </summary>
+    class Limitation
+    {
+        public List<Limitation> Limitations { get; private set; }
+        public Func<Vector2, double> Formula { get; private set; }                
+    }
+}
